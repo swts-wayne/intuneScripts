077a96186dee0f35fc353c44e40d09c6cee40359
Wayne <wayne.cogan@swtafe.edu.au>
2026-09-08 10:07:02 +1000

    Cleanup!
    Added GitHub Desktop winget app and started to sort script types into folders

---
+++ Winget Apps/GitHub Desktop/uninstall.ps1
@@ -0,0 +1,19 @@
+#Uninstall script for Local Send via Winget
+$ErrorActionPreference = "Stop"
+
+$PackageName  = "GitHub.GitHubDesktop"
+
+# Resolve winget.exe
+$Winget = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe"
+if ($Winget.count -gt 1) {
+    $Winget = $Winget[-1].Path
+}
+
+if (!$Winget) {
+    Write-Error "winget not installed"
+} else {
+    # uninstall via winget
+    & $Winget uninstall --silent --exact --id $PackageName
+}
+
+exit $LASTEXITCODE
